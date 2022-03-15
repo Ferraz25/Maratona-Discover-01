5365dff0f7366598439eeced834971602cbfb353
Ferraz25 <victorferraz244@gmail.com>
2022-03-14 22:32:38 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,212 @@
+const Modal = {
+  open() {
+    //Abrir modal
+    //Adicionar a class active ao modal
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    //Fechar o modal
+    //Remover a class active do modal
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
+
+const Transaction = {
+  all: [
+    {
+      description: 'Luz',
+      amount: -50000,
+      date: '23/01/2021'
+    },
+    {
+      description: 'Website',
+      amount: 500000,
+      date: '23/01/2021'
+    },
+    {
+      description: 'Internet',
+      amount: -20000,
+      date: '23/01/2021'
+    },
+    {
+      description: 'App',
+      amount: 200000,
+      date: '23/01/2021'
+    }
+  ],
+
+  add(transaction) {
+    Transaction.all.push(transaction)
+
+    App.reload()
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1)
+
+    App.reload()
+  },
+
+  incomes() {
+    let income = 0
+
+    //pegar todas as transações
+    //para cada transação,
+    Transaction.all.forEach(transaction => {
+      //se for maior que zero
+      if (transaction.amount > 0) {
+        //somar a uma variável e retornar a variável
+        income += transaction.amount
+      }
+    })
+    return income
+  },
+
+  expenses() {
+    let expense = 0
+
+    //pegar todas as transações
+    //para cada transação,
+    Transaction.all.forEach(transaction => {
+      //se for menor que zero
+      if (transaction.amount < 0) {
+        //somar a uma variável e retornar a variável
+        expense += transaction.amount
+      }
+    })
+    return expense
+  },
+
+  total() {
+    return Transaction.incomes() + Transaction.expenses()
+  }
+}
+
+//Substituir os dados do HTML com os dados do JS
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `
+
+    return html
+  },
+
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    )
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    )
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    )
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ''
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    //verificando se o valor é positivo ou negativo
+    const signal = Number(value) < 0 ? '-' : ''
+
+    //trabalha na remoção de algum caracter especial
+    value = String(value).replace(/\D/g, '')
+
+    //divide o numero por cem para ficar formato com 3 casas decimais, como na nossa moeda
+    value = Number(value) / 100
+
+    //formata em dinheiro, acresentando o cifrão e a vírgula
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + value
+  }
+}
+
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues()
+
+    if (
+      description.trim() === '' ||
+      amount.trim() === '' ||
+      date.trim() === ''
+    ) {
+      throw new Error('Por favor, preencha todos os campos')
+    }
+  },
+
+  submit(event) {
+    event.preventDefault()
+
+    try {
+      //verificar se todas as informações foram preenchidas
+      Form.validateFields()
+
+      //formatar os dados para salvar
+      // Form.FormData()
+
+      //salvar
+      //apagar os dados do forumulário
+      //modal feche
+      //Atualizar a aplicação
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+}
+
+const App = {
+  init() {
+    Transaction.all.forEach(transaction => {
+      DOM.addTransaction(transaction)
+    })
+
+    DOM.updateBalance()
+  },
+  reload() {
+    DOM.clearTransactions()
+    App.init()
+  }
+}
+
+App.init()
+
+// Transaction.remove(0)
